81d46350113391dc1813ddd5961ebe13683f85fb
agnesvarga1 <agikolyok@gmail.com>
2024-01-12 18:09:39 +0100

    snack1 + cooments snack2 happy pass done

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,31 +2,41 @@
 // snack 1:
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
 
-//declara un arr vuoto
 //chiedi utentente un numero tramitte prompt
 //salva in var
 //condizione se il numero piu di 50
 //if NOT ripeti prompt e
-//push in arr
+
 //somma i valori input
 //else stampa numeri
 //esco dal loop se numero dato > 50 or al interno del arr la somma
-const arrNums = [];
 
+//declara un arr vuoto
+const arrNums = [];
+//variabile init
 let num = 0;
+//var aiutante
 let sum = 0;
+
 do {
+  //chiedi utente digitare un numero
+  //se numero e minore di 50 ripeti prompt
+
   do {
+    //chiedi utente un numero finche non digita un vero num
     num = Number(prompt("Inserisci un numero"));
   } while (isNaN(num));
+  //push in arr
   arrNums.push(num);
   console.log(arrNums);
   let i = 0;
-
+  //prendi elementi del arr
   while (i < arrNums.length) {
+    //somma dei elmenti
     sum = sum + arrNums[i];
     i++;
   }
+  //num diventa somma cosi se sum > 50 esco dal loop
   num = sum;
   sum = 0;
   console.log(num);
--- snack-2/script.js
+++ snack-2/script.js
@@ -1,1 +1,24 @@
-alert("ehi");
+// snack 2
+// genera un numero casuale tra 1 e 100 e poi chiedi all'utente di indovinare il numero. Il programma poi deve far comparire un messaggio "il numero è troppo alto" oppure "il numero è troppo basso" nel momento in cui il numero detto dall'utente non è perfettamente uguale al numero estratto in modo casuale
+function getRandomNumber(min, max) {
+  let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
+
+let number = getRandomNumber(1, 100);
+let userNumber;
+console.log(number);
+console.log(userNumber);
+userNumber = Number(prompt("indovina il numero fra 1 e 100?"));
+while (userNumber !== number) {
+  if (userNumber > number) {
+    alert(`${userNumber} e troppo alto `);
+    userNumber = Number(prompt("indovina il numero fra 1 e 100?"));
+  } else if (userNumber < number) {
+    alert(`${userNumber} e troppo basso `);
+    userNumber = Number(prompt("indovina il numero fra 1 e 100?"));
+  } else {
+    document.writeln(`<h2>hai indovinato il numero</h2>`);
+  }
+}
+document.writeln(`<h2>hai indovinato il numero pensto = ${number}</h2>`);
