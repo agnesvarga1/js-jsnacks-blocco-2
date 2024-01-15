a707572c9d1849f854b506a64b68355c91c164ed
agnesvarga1 <agikolyok@gmail.com>
2024-01-15 15:04:17 +0100

    fix snack 4

--- snack-3/script.js
+++ snack-3/script.js
@@ -7,12 +7,19 @@
 //2) ripeti aggiunta elemento finche .length non e uguale
 
 const arr1 = ["pippo", 5, "unaltroelement", "agnes", "55", "Budapest"];
-const arr2 = ["pluto", 5, "unaltroelement"];
+const arr2 = [
+  "pluto",
+  5,
+  "unaltroelement",
+  "elemento1",
+  "elemento2",
+  "elementoXY, 99",
+];
 
 let elementToInsert;
 
 if (arr1.length < arr2.length) {
-  for (let i = arr1.length; i < arr2.length + 1; i++) {
+  for (let i = arr1.length; i < arr2.length; i++) {
     elementToInsert = prompt("Add an element to arr1");
     arr1.push(elementToInsert);
   }
@@ -24,6 +31,7 @@ if (arr1.length < arr2.length) {
 } else {
   console.log("arrs uguali");
 }
-
-document.writeln(arr1);
-document.writeln(arr2);
+console.log(arr1.length);
+console.log(arr2.length);
+document.writeln(`<h2>Array 1 [${arr1}]</h2>`);
+document.writeln(`<h2>Array 2 [${arr2}]</h2>`);
--- snack-4/script.js
+++ snack-4/script.js
@@ -3,12 +3,18 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 let unNumero;
 
-do {
-  unNumero = Number(prompt("Iserisci un numero"));
-} while (isNaN(unNumero));
+function getUserNumber() {
+  do {
+    unNumero = Number(prompt("Iserisci un numero"));
+  } while (isNaN(unNumero));
+  return unNumero;
+}
 
-if (unNumero % 2 === 0) {
-  document.writeln(unNumero);
-} else {
-  document.writeln(unNumero + 1);
+for (let i = 0; i < 5; i++) {
+  getUserNumber();
+  if (unNumero % 2 === 0) {
+    document.writeln(`<p>numero pari:  ${unNumero}</p>`);
+  } else {
+    document.writeln(`<p>Numero dispari + 1: ${unNumero + 1}</p>`);
+  }
 }
